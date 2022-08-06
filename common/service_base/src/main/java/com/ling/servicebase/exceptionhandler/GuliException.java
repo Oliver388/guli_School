dbcc8b2a3b6b9455b91793dee472d151ecb7f7a3
Oliver388 <oliverling12345@gmail.com>
2022-08-06 10:29:04 +0800

    完善功能

--- common/service_base/src/main/java/com/ling/servicebase/exceptionhandler/GuliException.java
+++ common/service_base/src/main/java/com/ling/servicebase/exceptionhandler/GuliException.java
@@ -1,5 +1,6 @@
 package com.ling.servicebase.exceptionhandler;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor//生成无参构造
 public class GuliException extends RuntimeException{
 
+    @ApiModelProperty(value = "状态码")
     private Integer code;
 
     private String msg;
